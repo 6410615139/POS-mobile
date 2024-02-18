b23422a8e0eda6ebed9cdb81a48aaf52713dfcee
Supakrit Nithikethkul <6410615139@student.tu.ac.th>
2024-02-18 15:57:44 +0700

    come back

--- react/POS/App.js
+++ react/POS/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello POS worker</Text>
+      <Text>Test POS</Text>
       <StatusBar style="auto" />
     </View>
   );
