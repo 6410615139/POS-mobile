4983d40284d9fa05a97d7210774171a8b01da0e9
Chalisa Thummaraj <6410685041@student.tu.ac.th>
2024-03-17 00:16:32 +0700

    Edit Profile

--- POS/POS/ViewModels/ProfileViewModel.swift
+++ POS/POS/ViewModels/ProfileViewModel.swift
@@ -1,16 +1,119 @@
+////
+////  ProfileViewModel.swift
+////  POS
+////
+////  Created by ชลิศา ธรรมราช on 14/3/2567 BE.
+////
 //
-//  ProfileViewModel.swift
-//  POS
+//import Foundation
+//import FirebaseAuth
+//import FirebaseFirestore
 //
-//  Created by ชลิศา ธรรมราช on 14/3/2567 BE.
+//class ProfileViewModel {
+//    var user: User = User(id: "", name: "", email: "", joined: 0.0)
 //
+//    func fetchUserName() -> String {
+//        guard let user = Auth.auth().currentUser else {
+//            print("No authenticated user found")
+//            return ""
+//        }
+//
+//        let db = Firestore.firestore()
+//        let userRef = db.collection("users").document(user.uid)
+//
+//        userRef.getDocument { (document, error) in
+//            if let document = document, document.exists {
+//                let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+//                print("Document data: \(dataDescription)")
+//                if let userName = document.get("name") as? String {
+//                    print("User name: \(userName)")
+//                    return userName
+//                } else {
+//                    print("User name not found")
+//                }
+//            } else {
+//                print("Document does not exist")
+//            }
+//        }
+//        return ""
+//    }
+//
+//}
+//
+//import Foundation
+//import FirebaseAuth
+//import FirebaseFirestore
+//import Combine
+//
+//class ProfileViewModel: ObservableObject {
+//    @Published var user: User
+//
+//    init() {
+//        guard let firebaseUser = Auth.auth().currentUser else {
+//            print("No authenticated user found")
+//            return
+//        }
+//
+//        let db = Firestore.firestore()
+//        let userRef = db.collection("users").document(firebaseUser.uid)
+//
+//        userRef.getDocument { [weak self] (document, error) in
+//            if let document = document, document.exists {
+//                if let userData = document.data() {
+//                    let id = firebaseUser.uid
+//                    let name = userData["name"] as? String ?? ""
+//                    let email = userData["email"] as? String ?? ""
+//                    let joined = userData["joined"] as? TimeInterval ?? 0.0
+//              //      DispatchQueue.main.async {
+//                    self!.user = User(id: id, name: name, email: email, joined: joined)
+//                    print(name)
+//                    print(self!.user.name)
+//                        
+//                 //   }
+//                } else {
+//                    print("User name not found")
+//                }
+//            } else {
+//                print("Document does not exist")
+//            }
+//        }
+//    }
+//}
 
 import Foundation
 import FirebaseAuth
 import FirebaseFirestore
 
-class ProfileViewModel {
-    let user = Auth.auth().currentUser
-
+class ProfileViewModel: ObservableObject {
+    @Published var user: User? = nil
     
+    func fetchUser() {
+        guard let userId = Auth.auth().currentUser?.uid else {
+            return
+        }
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(userId).getDocument { [weak self] snapshot, error in
+                guard let data = snapshot?.data(), error == nil else {
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    self?.user = User(
+                        id: data["id"] as? String ?? "",
+                        name: data["name"] as? String ?? "",
+                        email: data["email"] as? String ?? "",
+                        joined: data["joined"] as? TimeInterval ?? 0
+                    )
+                }
+            }
+    }
+    
+    func logOut() {
+        do {
+            try Auth.auth().signOut()
+        } catch {
+            print(error)
+        }
+    }
 }
--- POS/POS/Views/ProfileView.swift
+++ POS/POS/Views/ProfileView.swift
@@ -8,22 +8,79 @@
 import SwiftUI
 
 struct ProfileView: View {
-    var viewModel = ProfileViewModel()
+    @StateObject var viewModel = ProfileViewModel()
+    let layout = [
+        GridItem(.fixed(60)),
+        GridItem(.flexible(minimum: 40), alignment: .leading)
+    ]
     
     var body: some View {
         ZStack{
-            RoundedRectangle(cornerRadius: 20)
-                .fill(Color(UIColor(hex: "#b31464")))
-                .frame(width: .infinity, height: 550)
-            RoundedRectangle(cornerRadius: 20)
-                .fill(Color(UIColor(hex: "#fecede")))
-                .frame(width: .infinity, height: 500)
-                .padding(.top, 60)
-            VStack{
-                Text("")
+            if let user = viewModel.user {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Color(UIColor(hex: "#b31464")))
+                    .frame(width: .infinity, height: 550)
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color(UIColor(hex: "#fecede")))
+                    .frame(width: .infinity, height: 500)
+                    .padding(.top, 60)
+                VStack{
+                    HStack{
+                        Spacer()
+                        Image(systemName: "person.circle.fill")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 170)
+                            .foregroundColor(.white)
+                    }
+                    Spacer()
+                        .frame(height: 500)
+                }
+                VStack(alignment: .leading){
+                    HStack{
+                        Text("STAFF")
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "gearshape.fill")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 20)
+                                .foregroundColor(.white)
+                        }
+                    }
+                    Text("< position >")
+                    Spacer()
+                        .frame(height: 100)
+                    Text("Name:")
+                    Text(user.name)
+                        .padding(10)
+                        .frame(width: 300)
+                        .border(.white, width: 3)
+                    LazyVGrid(columns: layout, content: {
+                        Text("Gmail: ")
+                        ScrollView(.horizontal){
+                            Text(user.email)
+                        }
+                        Text("Tel: ")
+                        Text("mytel")
+                    })
+                    Button {
+                        
+                    } label: {
+                        Text("History")
+                    }
+                }
+                .padding(30)
+                .font(.system(size: 20))
+            } else {
+                Text("Loading Profile")
             }
-        }
-        .padding(20)
+    }
+    .padding(20)
+    .onAppear {
+        viewModel.fetchUser()
+    }
     }
 }
 
