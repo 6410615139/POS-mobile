991500b563b6a68de400eb8e094bcbd8f98301e7
Pond <6410685033@student.tu.ac.th>
2024-04-05 15:20:46 +0700

    implement login logic

--- POS/POS/Views/LoginView.swift
+++ POS/POS/Views/LoginView.swift
@@ -8,9 +8,7 @@
 import SwiftUI
 
 struct LoginView: View {
-    
-    @State var email = ""
-    @State var password = ""
+    @StateObject var viewModel = LoginViewModel()
     
     var body: some View {
         NavigationView {
@@ -18,12 +16,14 @@ struct LoginView: View {
                 HeaderView(title: "POS", subtitle: "Point of Sale", angle: 15, background: .pink)
                 
                 Form {
-                    TextField("Email Address", text: $email)
+                    TextField("Email Address", text: $viewModel.email)
                         .textFieldStyle(DefaultTextFieldStyle())
-                    SecureField("Password", text: $password)
+                        .autocorrectionDisabled()
+                        .autocapitalization(.none)
+                    SecureField("Password", text: $viewModel.password)
                         .textFieldStyle(DefaultTextFieldStyle())
                     POSButton(title: "Log In", background: .blue) {
-                        
+                        viewModel.login()
                     }
                 }
                 VStack {
