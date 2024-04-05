4f1eafa05ff3785c40046c07ea8cdade9c16794f
Chalisa Thummaraj <6410685041@student.tu.ac.th>
2024-04-05 16:22:59 +0700

    Edit EditProfile

--- POS/POS/ViewModels/ProfileViewModel.swift
+++ POS/POS/ViewModels/ProfileViewModel.swift
@@ -1,84 +1,9 @@
-////
-////  ProfileViewModel.swift
-////  POS
-////
-////  Created by ชลิศา ธรรมราช on 14/3/2567 BE.
-////
 //
-//import Foundation
-//import FirebaseAuth
-//import FirebaseFirestore
+//  ProfileViewModel.swift
+//  POS
 //
-//class ProfileViewModel {
-//    var user: User = User(id: "", name: "", email: "", joined: 0.0)
+//  Created by ชลิศา ธรรมราช on 14/3/2567 BE.
 //
-//    func fetchUserName() -> String {
-//        guard let user = Auth.auth().currentUser else {
-//            print("No authenticated user found")
-//            return ""
-//        }
-//
-//        let db = Firestore.firestore()
-//        let userRef = db.collection("users").document(user.uid)
-//
-//        userRef.getDocument { (document, error) in
-//            if let document = document, document.exists {
-//                let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
-//                print("Document data: \(dataDescription)")
-//                if let userName = document.get("name") as? String {
-//                    print("User name: \(userName)")
-//                    return userName
-//                } else {
-//                    print("User name not found")
-//                }
-//            } else {
-//                print("Document does not exist")
-//            }
-//        }
-//        return ""
-//    }
-//
-//}
-//
-//import Foundation
-//import FirebaseAuth
-//import FirebaseFirestore
-//import Combine
-//
-//class ProfileViewModel: ObservableObject {
-//    @Published var user: User
-//
-//    init() {
-//        guard let firebaseUser = Auth.auth().currentUser else {
-//            print("No authenticated user found")
-//            return
-//        }
-//
-//        let db = Firestore.firestore()
-//        let userRef = db.collection("users").document(firebaseUser.uid)
-//
-//        userRef.getDocument { [weak self] (document, error) in
-//            if let document = document, document.exists {
-//                if let userData = document.data() {
-//                    let id = firebaseUser.uid
-//                    let name = userData["name"] as? String ?? ""
-//                    let email = userData["email"] as? String ?? ""
-//                    let joined = userData["joined"] as? TimeInterval ?? 0.0
-//              //      DispatchQueue.main.async {
-//                    self!.user = User(id: id, name: name, email: email, joined: joined)
-//                    print(name)
-//                    print(self!.user.name)
-//                        
-//                 //   }
-//                } else {
-//                    print("User name not found")
-//                }
-//            } else {
-//                print("Document does not exist")
-//            }
-//        }
-//    }
-//}
 
 import Foundation
 import FirebaseAuth
--- POS/POS/Views/EditProfileView.swift
+++ POS/POS/Views/EditProfileView.swift
@@ -10,19 +10,30 @@ import SwiftUI
 struct EditProfileView: View {
     @StateObject var viewModel = ProfileViewModel()
     @Environment(\.presentationMode) var presentationMode
+    let layout = [
+        GridItem(.fixed(100), alignment: .leading),
+        GridItem(.flexible(minimum: 40), alignment: .leading)
+    ]
     
     var body: some View {
         if let user = viewModel.user {
             Form {
-                HStack{
-                    Text("Email:")
-                    Text(user.email)
-                }
-                TextField("Full Name", text: $viewModel.name)
-                    .textFieldStyle(DefaultTextFieldStyle())
-                TextField("Telephone", text: $viewModel.tel)
-                    .textFieldStyle(DefaultTextFieldStyle())
-                
+                LazyVGrid(columns: layout, content: {
+                    Text("Gmail:")
+                    ScrollView(.horizontal){
+                        Text(user.email)
+                            .foregroundStyle(Color.gray)
+                    }
+                    Text("Gender:")
+                    Text(user.gender)
+                        .foregroundStyle(Color.gray)
+                    Text("Full Name:")
+                    TextField("Full Name:", text: $viewModel.name)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    Text("Telephone:")
+                    TextField("Telephone", text: $viewModel.tel)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                })
                 POSButton(title: "Save", background: .green) {
                     viewModel.edit()
                     self.presentationMode.wrappedValue.dismiss()
