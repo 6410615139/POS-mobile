321813c23e6cb63e0d05c828c949533068f8e8ac
Supakrit Nithikethkul <6410615139@student.tu.ac.th>
2024-04-25 23:31:14 +0700

    menu search bar

--- POS/POS/Views/Bill/MenuView.swift
+++ POS/POS/Views/Bill/MenuView.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Supakrit Nithikethkul on 9/4/2567 BE.
 //
+
 import SwiftUI
 import FirebaseFirestoreSwift
 import FirebaseFirestore
@@ -11,11 +12,11 @@ import FirebaseAuth
 
 struct MenuView: View {
     @StateObject var viewModel: MenuViewModel
-    @FirestoreQuery(collectionPath: "products") var items: [Product]
+    @FirestoreQuery(collectionPath: "products") var allItems: [Product]
     @State private var billId: String
     @State private var table: String = ""
     @State private var owner: String = ""
-    @State private var isBillDetailsActive = false
+    @State private var searchQuery = ""  // For product search
 
     private var columns: [GridItem] = [
         GridItem(.flexible(), spacing: 20),
@@ -29,17 +30,40 @@ struct MenuView: View {
         self.billId = billId
     }
 
+    // Filtered items based on search query
+    var filteredItems: [Product] {
+        if searchQuery.isEmpty {
+            return allItems
+        } else {
+            return allItems.filter { item in
+                item.product_name.lowercased().contains(searchQuery.lowercased())
+            }
+        }
+    }
+
     var body: some View {
         NavigationView {
             ScrollView {
                 VStack(alignment: .leading) {
-                    Text("Table")
-                    TextField("Enter table number", text: $table)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .padding()
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("Table")
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                            TextField("Table Number", text: $table)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                        }
+                        VStack(alignment: .leading) {
+                            Text("Owner")
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                            TextField("Owner's Name", text: $owner)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                        }
+                    }
+                    .padding()
 
-                    Text("Owner")
-                    TextField("Enter owner's name", text: $owner)
+                    TextField("Search Products...", text: $searchQuery)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .padding()
 
@@ -47,12 +71,13 @@ struct MenuView: View {
                         updateBillDetails()
                     }
                     .padding()
+                    .frame(maxWidth: .infinity)
                     .background(Color.blue)
                     .foregroundColor(.white)
                     .cornerRadius(8)
 
                     LazyVGrid(columns: columns, spacing: 20) {
-                        ForEach(items) { item in
+                        ForEach(filteredItems, id: \.id) { item in
                             MenuItemView(billId: billId, product: item)
                                 .frame(width: itemSize.width, height: itemSize.height)
                                 .background(Color(UIColor.systemBackground))
@@ -71,8 +96,6 @@ struct MenuView: View {
     }
 
     private func fetchBillDetails() {
-        // Fetch the current details of the bill using the billId
-        // Assume using Firestore to fetch the data and initialize table and owner
         Firestore.firestore().collection("bills").document(billId).getDocument { document, error in
             if let document = document, document.exists {
                 let data = document.data()
@@ -85,7 +108,6 @@ struct MenuView: View {
     }
 
     private func updateBillDetails() {
-        // Update the table and owner information in Firestore
         let billRef = Firestore.firestore().collection("bills").document(billId)
         billRef.updateData([
             "table": table,
